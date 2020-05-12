7c7bd5b6132ad63e44d7f1d27f0219844c131dc3
r-kimura001 <torakichisite@gmail.com>
2020-05-12 14:18:38 +0900

    feat: ユーザー登録

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = '登録に成功しました'
+      # ↓redirect_to user_url(@user)と等価。user_urlとは、「 /users/:id」で、rails routesでPrefixを確認するとわかる
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
