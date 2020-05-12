d5a562120f1daf67f2b90a6dde867448c23714be
r-kimura001 <torakichisite@gmail.com>
2020-05-12 16:07:31 +0900

    feat: セッションによるログイン、ログアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = '登録に成功しました'
+      log_in @user
       # ↓redirect_to user_url(@user)と等価。user_urlとは、「 /users/:id」で、rails routesでPrefixを確認するとわかる
       redirect_to @user
     else
