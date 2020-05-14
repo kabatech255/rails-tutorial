276e8c61714ebe3e5ce1f9e0f8fbe8cf3e3a3772
r-kimura001 <torakichisite@gmail.com>
2020-05-14 11:28:16 +0900

    ユーザーフォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+              foreign_key: "follower_id",
+              dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+              foreign_key: "followed_id",
+              dependent: :destroy
 
+  has_many :following, through: :active_relationships, source: :followed
   attr_accessor :remember_token
+
+  has_many :followers, through: :passive_relationships, source: :follower
 
   # DBに保存される前に、すべての文字列を小文字に変換する
   before_save { self.email = email.downcase }
@@ -48,6 +57,32 @@ class User < ApplicationRecord
 
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  # パスワード再設定の期限が切れている場合はtrueを返す
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
+  # ユーザーのステータスフィードを返す
+  def feed
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+  end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/help', to: 'static_pages#help'
-  resources :users
+
   resources :microposts, only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
